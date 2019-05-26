eb82a3e8e4f7ed1c11cde0cbe8bda728d93c353c
fut0k0 <cccabell@icloud.com>
2019-05-26 14:43:20 -0700

    refactored hashes into keyword arguments

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,10 @@
 class Dog
 	attr_accessor :name, :breed, :id
 	
-	def initialize(data)
-		@id = nil
-		@name = data[:name]
-		@breed = data[:breed]
+	def initialize(id: nil, name:, breed:)
+		@id = id
+		@name = name
+		@breed = breed
 	end
 	
 	def self.create_table
@@ -24,21 +24,19 @@ class Dog
 	end
 	
 	def self.new_from_db(row)
-		dog = self.new({:name => row[1], :breed => row[2]})
-		dog.id = row[0]
-		dog
+		self.new({id: row[0], name: row[1], breed: row[2]})
 	end
 	
 	def self.find_by_name(name)
 		sql = "SELECT * FROM dogs WHERE name = ? LIMIT 1"
-		result = DB[:conn].execute(sql, name) [0]
-		self.new_from_db(result)
+		row = DB[:conn].execute(sql, name) [0]
+		self.new_from_db(row)
 	end
 	
 	def self.find_by_id(id)
 		sql = "SELECT * FROM dogs WHERE id = ?"
-		result = DB[:conn].execute(sql, id) [0]
-		self.new_from_db(result)
+		row = DB[:conn].execute(sql, id) [0]
+		self.new_from_db(row)
 	end
 	
 	def update
@@ -51,29 +49,33 @@ class Dog
 	end
 	
 	def save
-		sql = <<~SQL
-					INSERT INTO dogs (name, breed)
-					VALUES (?, ?)
-					SQL
-		DB[:conn].execute(sql, self.name, self.breed)
-		self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+		if self.id
+			self.update
+		else
+			sql = <<~SQL
+						INSERT INTO dogs (name, breed)
+						VALUES (?, ?)
+						SQL
+			DB[:conn].execute(sql, self.name, self.breed)
+			@id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+		end
 		self
 	end
 	
-	def self.create(data)
-		dog = self.new(data)
+	def self.create(name:, breed:)
+		dog = self.new(name: name, breed: breed)
 		dog.save
 	end
 	
-	def self.find_or_create_by(data)
+	def self.find_or_create_by(name:, breed:)
 		sql = <<~SQL
 					SELECT * FROM dogs
 					WHERE name = ? AND breed = ?
 					SQL
-		row = DB[:conn].execute(sql, data[:name], data[:breed])
+		row = DB[:conn].execute(sql, name, breed)
 		
 		if row.empty?
-			self.create(data)
+			self.create(name: name, breed: breed)
 		else
 			self.new_from_db(row[0])
 		end
